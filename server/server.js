a481290c8e316468161cd851296b7121fc9eb446
Nantawan G <toeinajana@hotmail.com>
2019-06-13 18:53:21 +1000

    "working on backen"

--- server/server.js
+++ server/server.js
@@ -5,8 +5,13 @@ const server = require("http").createServer(app);
 const io = require('socket.io').listen(server);
 const port = 5000;
 
+// let Users = require('./user.model');
+
 // connect to mongo
 mongo.connect('mongodb://127.0.0.1:27017/appchat', function (err, db) {
+
+   var db = db.db('appchat');
+
     if (err) {
         throw err;
     }
@@ -14,12 +19,17 @@ mongo.connect('mongodb://127.0.0.1:27017/appchat', function (err, db) {
     console.log('Mongo connected');
 
 
+
     io.on("connection", socket => {
-        console.log("a user connected XD");
+
+       
+
+        console.log("a user connected to socket.io");
 
         //listen to message
 
         socket.on("chat message", mgs => {
+
             //display messages
 
             console.log(mgs);
@@ -33,67 +43,8 @@ mongo.connect('mongodb://127.0.0.1:27017/appchat', function (err, db) {
         })
     });
 
+  });
+
     server.listen(port, () => console.log("server running on port : " + port));
 
-    // connect socket
-    // io.on('connection', socket = > function(){
-    //     let chat = db.collection('chats');
-
-    //     //create function to sent status
-    //     sendStatus = function(s){
-    //         socket.emit('status', s);
-    //     }
-
-    //     // get chat from mongo -- limit 100 chat
-
-    //     chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
-
-    //         if(err){
-    //             throw err;
-    //         }
-
-    //         //emit messages
-    //         socket.emit('output', res);
-    //     });
-
-    //     //handle input events
-    //     socket.on('input', function(data){
-    //         let name = data.name;
-    //         let message = data.message;
-
-    //         // chaeck name and message
-
-    //         if(name == '' || message == ''){
-
-    //             sendStatus('Please enter sth');
-    //         }
-    //         else {
-
-    //             //insert message
-    //             chat.insert({name: name, message: message}, function(){
-    //                 io.emit('output', [data]);
-
-    //                 // send status obj
-
-    //                 sendStatus({
-    //                     message: 'Message has sent',
-    //                     clear: true
-    //                 });
-    //             });
-    //         }
-    //     });
-
-    // // handle clrs
-    // socket.on('clear', function(data){
-    //     //remove all chat
-    //     chat.remove({}, function(){
-    //         // emit cls and let the user know
-    //         socket.emit('all cleared');
-    //     });
-    // });
-
-    // });
-
-
-
-});
+    
